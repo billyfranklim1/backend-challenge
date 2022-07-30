64a5ed957605ad5d09d6999ebf0012ff9c530d9c
billyfranklim <billyfranklim@gmail.com>
2022-07-30 16:38:34 -0300

    feat: add endpoints api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'customer'], function () {
+    Route::get('/{id?}', [CustomerController::class, 'get']);
+    Route::post('/', [CustomerController::class, 'save']);
+    Route::put('/', [CustomerController::class, 'save']);
+    Route::delete('/', [CustomerController::class, 'destroy']);
+});
+
+
+Route::group(['prefix' => 'product'], function () {
+    Route::get('/{id?}', [ProductController::class, 'get']);
+    Route::post('/', [ProductController::class, 'save']);
+    Route::put('/', [ProductController::class, 'save']);
+    Route::delete('/', [ProductController::class, 'destroy']);
+});
+
+
+Route::group(['prefix' => 'order'], function () {
+    Route::get('/{id?}', [OrderController::class, 'get']);
+    Route::post('/', [OrderController::class, 'save']);
+    Route::put('/', [OrderController::class, 'save']);
+    Route::delete('/', [OrderController::class, 'destroy']);
 });
