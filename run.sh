67fc19ae6fef6e9847fe371c3ff52e9291fbba38
billyfranklim <billyfranklim@gmail.com>
2022-08-01 08:51:42 -0300

    feat: script run

---
+++ run.sh
@@ -0,0 +1,9 @@
+#chmod +x ./run.sh
+
+docker-compose up -d
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan migrate
+docker-compose exec app php artisan db:seed
+docker-compose exec app php artisan test
+
+echo "Access the app at http://127.0.0.1::8080"
