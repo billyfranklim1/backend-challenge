f3cd94bed9285a7ba1e0c9fe6024ccfa38b9218e
billyfranklim <billyfranklim@gmail.com>
2022-08-01 13:54:44 -0300

    fix: update readme

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #chmod +x ./run.sh
 
 docker-compose up -d
+docker-compose exec app php composer install
 docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan migrate
 docker-compose exec app php artisan db:seed
